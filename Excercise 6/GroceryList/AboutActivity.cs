680d2252c1c9e3e641b3723011c41f833c5a6477
Jimmy Tang <blay.tenshi@gmail.com>
2018-02-12 20:59:20 +1100

    Exercise 6 Completed

---
+++ Excercise 6/GroceryList/AboutActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace GroceryList
+{
+	[Activity(Label = "About")]			
+	public class AboutActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.About);
+
+			FindViewById<Button>(Resource.Id.learnMoreButton).Click += OnLearnMoreClick;
+		}
+
+		void OnLearnMoreClick(object sender, EventArgs e)
+		{
+            // Use below if you wanna be more explicit
+            // var intent = new Intent();
+            // intent.SetAction(Intent.ActionView);
+
+            // Gotta love them shortcuts
+            var intent = new Intent(Intent.ActionView);
+            intent.SetData(Android.Net.Uri.Parse("http://www.xamarin.com"));
+            StartActivity(intent);
+		}
+	}
+}
